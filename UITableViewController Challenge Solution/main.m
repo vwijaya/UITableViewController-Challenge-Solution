152ed2b3792992a7d8b2ad9c5bcaa47019254bfd
Valerino Wijaya <valerino@gmail.com>
2014-05-10 23:29:39 +0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Valerino on 5/10/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VWAppDelegate class]));
+    }
+}
